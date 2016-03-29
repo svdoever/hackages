ab1bc803b33e4b5b23fbbd91392eadffb084b19b
davy <davy.engonenze@crelan.be>
2016-03-29 13:59:37 +0200

    eslint integration

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import path from 'path';
+
+const entry = path.resolve(process.cwd(), 'index.js');
+
+const configuration = {
+  devtool: 'inline-source-map',
+  context: path.resolve(process.cwd(), './'),
+  entry: entry,
+  cache: false,
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, '../dist'),
+  },
+  resolve: {
+    root: path.resolve(__dirname, './'),
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js'],
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    hash: true,
+    versions: true,
+    timings: true,
+    chunks: true,
+    chunkModules: true,
+    cached: true,
+    cacheAssets: true,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        },
+        exclude: /(node_modules|bower_components)/,
+      },
+    ],
+  },
+};
+
+module.exports = configuration;
