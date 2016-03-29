f43c41a44b067b0776074323279552641df6512e
davy <davy.engonenze@crelan.be>
2016-03-29 15:08:56 +0200

    added dummy tests

---
+++ test/exec.spec.js
@@ -0,0 +1,17 @@
+const exec = require('../tools/exec.js');
+
+describe('Exec', function(){
+  // it('', () => {
+  //   expect(5).equal(2);
+  // });
+
+  it('should be a function');
+
+  it('should be called with at least one argument');
+
+  it('should throw an error if no arg is passed');
+
+  it('should throw an error if the first argument is not a function');
+
+  it('should execute the callback');
+});
