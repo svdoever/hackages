72634b9dd8583c4310ce2390d06d91b37ac6adde
davy <davy.engonenze@crelan.be>
2016-03-31 11:01:24 +0200

    added linter, karma

---
+++ tools/parser/karma.js
@@ -0,0 +1,21 @@
+/**
+* CLI tools for Crelan Bank
+**/
+
+import path from 'path';
+import cliparse from 'cliparse';
+import karma from '../karmaRunner';
+
+const testCMD = cliparse.command('test', {
+  description: 'Running unit tests with Karma',
+  options: [
+    cliparse.flag('watch', {
+      aliases: ['w'],
+      description: 'Enable auto watch'
+    }),
+  ]
+},
+karma);
+
+
+export default testCMD;
