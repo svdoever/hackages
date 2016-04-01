20c287ab77a5bfe774dc2302f5e895ea81c7b171
davy <davy.engonenze@crelan.be>
2016-04-01 11:25:47 +0200

    added publish script

---
+++ publish.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+const git = require('git-rev');
+const npm = require('npm');
+console.log('inside publish');
+
+
+git.branch(function(branch){
+  if (branch === 'develop') {
+    doPublish();
+  } else {
+    console.log('hey, you cannot push on something else than the develop branch');
+  }
+});
+
+
+function doPublish(){
+  npm.load({}, function(err) {
+    npm.commands.publish([], function(err) {
+      // npm.config.set("email", me.data.email, "user");
+      console.log(err || "Published to registry");
+      done(!err);
+    });
+    // npm.registry.adduser(me.data.username, me.data.password, me.data.email, function(err) {
+    //   if (err) {
+    //     console.log(err);
+    //     done(false);
+    //   } else {
+    //   }
+    // });
+  });
+}
