8eafc4e494ea71330e06fc95bc3b2e95185a8ba6
davyengone <engonenzedavy@yahoo.com>
2016-04-06 07:41:00 +0200

    exec.js is not really needed but you never know

--- tools/exec.js
+++ tools/exec.js
@@ -1,19 +1,21 @@
-const exec = (fn, options) => {
-  const task = fn.default || fn;
+(() => {
+  const exec = (fn, options) => {
+    const task = fn.default || fn;
 
-  return task(options).then(() => {
-    console.log(`${task.name} is done`);
-  });
-};
+    return task(options).then(() => {
+      console.log(`${task.name} is done`);
+    });
+  };
 
-const init = () => {}
-const args = process.argv[2];
+  const init = () => {}
+  const args = process.argv[2];
 
-if (args) {
-  const module = require(`./${args}`);
-  exec(module);
-} else {
-  throw new Error('Please provide a command to be executed...');
-}
+  if (args) {
+    const module = require(`./${args}`);
+    exec(module);
+  } else {
+    throw new Error('Please provide a command to be executed...');
+  }
 
-export default exec;
+  export default exec;
+});
