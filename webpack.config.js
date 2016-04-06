2bc40fec6d51c466d033004844ecfabcd22559d0
davyengone <engonenzedavy@yahoo.com>
2016-04-06 06:00:04 +0200

    fixed issue with node modules transform

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
-const path = require('path');
 
 
 const config = {
-  entry: './tools/parser/index.js',
   target: 'node',
-  externals: [nodeExternals()],
+  // externals: [nodeExternals()],
+  node: {
+    console: false,
+    global: false,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false,
+  },
+  entry: './tools/parser/index.js',
   output: {
     filename: 'index.js',
     path: path.join(__dirname, 'dist')
   },
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['', '.js', '.json']
   },
   plugins: [],
   module: {
@@ -30,6 +39,16 @@ const config = {
     ]
   }
 };
+
+config.externals = fs.readdirSync("node_modules")
+  .reduce(function(acc, mod) {
+    if (mod === ".bin") {
+      return acc
+    }
+
+    acc[mod] = "commonjs " + mod
+    return acc
+  }, {});
 
 if (process.env.NODE_ENV === 'production') {
   config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
