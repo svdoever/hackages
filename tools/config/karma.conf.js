cbf0acd4bf44292ec957ba7f58ca363e52511d00
davy <davy.engonenze@crelan.be>
2016-04-12 11:35:11 +0200

    excluded bower_comp and node_modules

--- tools/config/karma.conf.js
+++ tools/config/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file;
-const path = require('path');
+import path from 'path';
 const context = process.cwd();
 const nodeModules = path.resolve(__dirname, '../node_modules');
 
@@ -8,31 +8,31 @@ const localDeps = [
   'jasmine-promise-matchers/dist/jasmine-promise-matchers.js',
 ].map(file => path.join(nodeModules, file));
 
+console.log(path.resolve(__dirname));
 const externaleDeps = [
-  'test/*.spec.js',
-  'test/**/*.spec.js',
-  'scripts/**/test/**/*.spec.js'
+  'components/**/test/**/*.spec.js',
 ].map(file => path.join(context, file));
 
 // preprocessors configuration
-const testsFiles = path.resolve(context, 'test/**/*.spec.js');
-const scriptsFiles = path.resolve(context, 'scripts/**/test/**/*.spec.js');
+const testsFiles = path.resolve(context, 'components/**/**/test/**/*.spec.js');
+const scriptsFiles = path.resolve(context, 'components/**/**/scripts/**/*.js');
 
 const preprocessors = {};
 preprocessors[testsFiles] = ['webpack', 'coverage'];
 preprocessors[scriptsFiles] = ['webpack', 'coverage'];
 
-module.exports = function(config) {
+module.exports = (config) => {
   config.set({
     frameworks: ['jasmine'],
     files: localDeps.concat(externaleDeps),
     preprocessors: preprocessors,
-		webpackMiddleware: {
+    webpackMiddleware: {
         noInfo: true,
         stats: {
             colors: true
         }
     },
+    exclude: ['bower_components', 'node_modules'],
     reporters: ['mocha', 'coverage'],
     port: 9876,
     colors: true,
