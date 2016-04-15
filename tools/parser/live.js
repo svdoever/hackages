1344a1f7bf53b171dd731cd557f70ba09226bb80
davyengone <engonenzedavy@yahoo.com>
2016-04-15 05:05:16 +0200

    live-server command: use local instance from node_modules

--- tools/parser/live.js
+++ tools/parser/live.js
@@ -3,10 +3,11 @@
 **/
 import cliparse from 'cliparse';
 import exec from './exec';
+import config from '../config/configuration'
 
 const liveCMD = cliparse.command('live', {
   description: 'Starting local server on port 8000'
 },
-exec.bind(null, 'live-server'));
+exec.bind(null, config.liveCMD));
 
 export default liveCMD;
