42519389999e6a84566fa4cf1eae81faa90fa632
davyengone <engonenzedavy@yahoo.com>
2016-04-21 08:31:21 +0200

    fixed code style: lint

--- tools/parser/sass.js
+++ tools/parser/sass.js
@@ -2,11 +2,10 @@
 * Hackages CLI tools: Transpiling sass
 **/
 import cliparse from 'cliparse';
-import sass from './sassRunner';
 import exec from './exec';
 
 const sassCMD = cliparse.command('sass', {
-  description: 'Transpiling Sass files with node-sass'
+  description: 'Transpiling Sass files with node-sass',
 },
 exec.bind(null, 'sass --watch scss:css'));
 
