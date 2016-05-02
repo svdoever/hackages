35e7d7fe55a64beb7a890a0e39ddef72ecdfd26f
davyengone <engonenzedavy@yahoo.com>
2016-05-02 21:35:43 +0200

    using promise for spawn process

--- tools/parser/exec.js
+++ tools/parser/exec.js
@@ -4,25 +4,28 @@
 import { spawn } from 'child_process';
 import config from '../config/configuration';
 
-const execCMD = (command) => new Promise(() => {
+const execCMD = (command) => new Promise((resolve, reject) => {
   const options = {
     cwd: config.context,
     stdio: 'inherit',
     stdin: 'inherit',
   };
 
-  const cmd = spawn('node', [command], options);
+  const cmd =  spawn('node', [command], options);
 
   cmd.stdout.on('data', (data) => {
     console.log(data);
+    resolve(data);
   });
 
   cmd.stderr.on('data', (data) => {
     console.log(data);
+    resolve(data);
   });
 
   cmd.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
+    reject(code);
   });
 });
 
