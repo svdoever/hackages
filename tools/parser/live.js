d714255219873c27c6d69afa3645b77fd5409140
davyengone <engonenzedavy@yahoo.com>
2016-05-24 01:57:58 +0200

    fixe documentation for live-server

--- tools/parser/live.js
+++ tools/parser/live.js
@@ -6,7 +6,7 @@ import exec from './exec';
 import config from '../config/configuration';
 
 const liveCMD = cliparse.command('live', {
-  description: 'Starting local server on port 8000',
+  description: 'Starting local server on port 8080',
 },
 exec.bind(null, config.liveCMD));
 
